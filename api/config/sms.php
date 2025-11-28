227923313aa7e4b07482c28cc74be494fb871cc6
Charles Abraham <charlesabrahamr.work@gmail.com>
2025-11-28 11:41:46 +0530

    Update sms.php

--- api/config/sms.php
+++ api/config/sms.php
@@ -8,8 +8,8 @@ return [
     'service' => 'twilio',
 
     'twilio' => [
-        'account_sid' => 'AC18eb879a120f6685d2ac40c7d954ab97',
-        'auth_token' => '24d212fd773503b1287e4236ea209060',
-        'from_number' => '+16598370371'
+        'account_sid' => '',
+        'auth_token' => '',
+        'from_number' => ''
     ],
 ];
